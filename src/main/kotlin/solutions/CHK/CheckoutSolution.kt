023ffa301107aab408e8b31c07f7bdb3a69eb406
sbx_user1051 <sbx_user1051@169.254.55.141>
2024-10-05 20:03:56 +0000

    Sat Oct 05 20:03:56 UTC 2024

--- src/main/kotlin/solutions/CHK/CheckoutSolution.kt
+++ src/main/kotlin/solutions/CHK/CheckoutSolution.kt
@@ -2,15 +2,21 @@ package solutions.CHK
 
 object CheckoutSolution {
     fun checkout(skus: String): Int {
-        val items : Map<String, Int> = mapOf("A" to 50, "B" to 30, "C" to 20, "D" to 15)
+        val items : Map<String, Int> = mapOf("A" to 50, "B" to 30, "C" to 20, "D" to 15, "E" to 40)
         var totalValue = 0
 
+        // check for illegal values
         for (item in items) {
             if (!items.contains(item.toString())) return -1
         }
 
-        
+        val itemMap : MutableMap<String, Int> = mutableMapOf()
+
+        skus.forEach { item ->
+            itemMap[item.toString()] = itemMap.getOrDefault(item.toString(), 0) + 1
+        }
 
         return totalValue
     }
 }
+
