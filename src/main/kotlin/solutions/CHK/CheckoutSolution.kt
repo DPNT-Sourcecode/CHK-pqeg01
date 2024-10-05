15d41628318236fb05897d3de97f50212c776ed3
sbx_user1051 <sbx_user1051@169.254.55.141>
2024-10-05 20:15:56 +0000

    Sat Oct 05 20:15:56 UTC 2024

--- src/main/kotlin/solutions/CHK/CheckoutSolution.kt
+++ src/main/kotlin/solutions/CHK/CheckoutSolution.kt
@@ -14,8 +14,15 @@ object CheckoutSolution {
             itemMap[item.toString()] = itemMap.getOrDefault(item.toString(), 0) + 1
         }
 
-        val Ecount = itemMap.getOrDefault("E", 1)
+        // apply E deal
+        val Ediscount = itemMap.getOrDefault("E", 0).div(2)
+        itemMap["B"] = if (itemMap.getOrDefault("B", 0) - Ediscount < 0) {
+            0
+        } else {
+            itemMap.getOrDefault("B", 0) - Ediscount
+        }
 
         return totalValue
     }
 }
+
